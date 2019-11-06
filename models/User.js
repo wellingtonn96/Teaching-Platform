f4430533e3234f81ec876708ba31acc4303fcf3d
wellington santos <weltossousa@gmail.com>
2019-11-06 10:19:07 -0300

    codes

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('../db/dbConnection')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -20,6 +21,12 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+})
+
+UserSchema.pre('save', async function (next){
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+    next()
 })
 
 const User = mongoose.model('User', UserSchema)
